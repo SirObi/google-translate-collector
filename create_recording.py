e348d5ba96d9ddb074549dd098b6996608212ce7
SirObi <leszek.orciuch@gmail.com>
2018-07-28 11:22:42 +0100

    Turn mp3 files into one recording with spaces

--- create_recording.py
+++ create_recording.py
@@ -1,4 +1,6 @@
+import os
 from pydub import AudioSegment
+from itertools import repeat
 
 def concatenate_recording(dir_name, *args):
     '''Create a recording given an arbitrarily long
@@ -11,15 +13,27 @@ def concatenate_recording(dir_name, *args):
     silence = AudioSegment.from_mp3(silence_file)
     
     final_output = silence + silence + silence
-
+    
+    two_secs = create_pause(silence, 2)
     for audio_file in args:
-        print(audio_file)
         next_phrase = AudioSegment.from_mp3(audio_file)
-        final_output = final_output + next_phrase + silence
+        pause_required = int(round(next_phrase.duration_seconds))
+        print(str(pause_required))
+        pause = create_pause(silence, pause_required) + two_secs
+        final_output = final_output + next_phrase + pause
     
     final_output.export(output_file, format="mp3")
 
+
+def create_pause(pause, duration_required_sec):
+    final_pause = pause
+    for i in range(duration_required_sec):
+        final_pause += pause
+    return final_pause
+
 # Example usage:
+files_list = ["./english_words/individual_recordings/" + file for file in os.listdir("./english_words/individual_recordings/")]
+concatenate_recording("english_words2", *files_list)
 concatenate_recording("some_dir4", *["./english_words/individual_recordings/How_are_you.mp3", "./english_words/individua\
 l_recordings/Cat.mp3"])
 
