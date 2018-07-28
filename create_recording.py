d111c79f6207c86dcbfd6fbb8bb4e8c7c9707155
Sara Radenovic <saradenovic@gmail.com>
2018-07-28 10:48:46 +0100

    Use pydub

--- create_recording.py
+++ create_recording.py
@@ -1,22 +1,25 @@
-def concatenate_recording(dir_name, silence_dir,  *args):
+from pydub import AudioSegment
+
+def concatenate_recording(dir_name, *args):
     '''Create a recording given an arbitrarily long
     list of mp3 files'''
     
     output_file = './{}.mp3'.format(dir_name)
     print("This is output file: {}".format(output_file))
     
-    silence_file = './silence_mp3s/Silence08s.mp3'
-    with open(silence_file, 'rb') as silence_2sec:
-        silence = silence_2sec.read()
+    silence_file = './silence_mp3s/Silence01s.mp3'
+    silence = AudioSegment.from_mp3(silence_file)
+    
+    final_output = silence + silence + silence
 
-    with open(output_file, 'ab') as outfile:
-        for audio_file in args:
-            with open(audio_file, 'rb') as infile:
-                outfile.write(silence)
-                input = infile.read()
-                outfile.write(input)
-
+    for audio_file in args:
+        print(audio_file)
+        next_phrase = AudioSegment.from_mp3(audio_file)
+        final_output = final_output + next_phrase + silence
+    
+    final_output.export(output_file, format="mp3")
 
 # Example usage:
-concatenate_recording("some_dir3", *["./english_words/individual_recordings/Hello.mp3", "./english_words/individual_recordings/Cat.mp3"])
+concatenate_recording("some_dir4", *["./english_words/individual_recordings/How_are_you.mp3", "./english_words/individua\
+l_recordings/Cat.mp3"])
 
