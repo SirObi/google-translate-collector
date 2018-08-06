cf5cf6bfdd33deb0b9687ce6ef555c85ed30a166
SirObi <leszek.orciuch@gmail.com>
2018-08-06 09:53:53 +0100

    Clean up create_recording.py

--- create_recording.py
+++ create_recording.py
@@ -2,6 +2,9 @@ import os
 from pydub import AudioSegment
 from itertools import repeat
 
+MP3_OUTPUTS = "./mp3_outputs/Mandarin vocab - 1/individual_recordings/"
+OUTPUT_DIR = "./mp3_outputs/Mandarin vocab - 1"
+
 def concatenate_recording(dir_name, *args):
     '''Create a recording given an arbitrarily long
     list of mp3 files'''
@@ -18,7 +21,6 @@ def concatenate_recording(dir_name, *args):
     for audio_file in args:
         next_phrase = AudioSegment.from_mp3(audio_file)
         pause_required = int(round(next_phrase.duration_seconds))
-        print(str(pause_required))
         pause = create_pause(silence, pause_required) + two_secs
         final_output = final_output + next_phrase + pause
     
@@ -32,8 +34,5 @@ def create_pause(pause, duration_required_sec):
     return final_pause
 
 # Example usage:
-files_list = ["./english_words/individual_recordings/" + file for file in os.listdir("./english_words/individual_recordings/")]
-concatenate_recording("english_words2", *files_list)
-concatenate_recording("some_dir4", *["./english_words/individual_recordings/How_are_you.mp3", "./english_words/individua\
-l_recordings/Cat.mp3"])
-
+files_list = [MP3_OUTPUTS + file for file in os.listdir(MP3_OUTPUTS)]
+concatenate_recording(OUTPUT_DIR, *files_list)
