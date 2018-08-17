c1372916e078ce6a6296a8221b96a7af5621ef5d
SirObi <leszek.orciuch@gmail.com>
2018-08-17 12:26:52 +0100

    Only run csv_json if main

--- csv_json.py
+++ csv_json.py
@@ -2,17 +2,19 @@ import csv
 import json
 import os
 
-FILE_PATH = './csv_inputs/Mandarin vocab - 1.csv'
+FILE_PATH = './csv_inputs/Lesson 14-08-2018 - Phrasebook.csv'
 
 def save_csv_to_json(file_path):
    with open(file_path) as csvfile:
         reader = csv.DictReader(csvfile)
-        
+
         output_path = os.path.splitext(csvfile.name)[-2].replace('csv_inputs', 'json_files') + '.json'
         print("Output file: {}".format(output_path))
 
         output_dict = {row['English']: row['Hanzi'] for row in reader}
         with open(output_path, 'w') as output_file:
            json.dump(output_dict, output_file)
+        return output_path
 
-save_csv_to_json(FILE_PATH)
+if __name__ == '__main__':
+    save_csv_to_json(FILE_PATH)
