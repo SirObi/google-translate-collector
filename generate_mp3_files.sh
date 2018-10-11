30a59ac519c3d4f7fed836437e09e85a7f2f8843
SirObi <leszek.orciuch@gmail.com>
2018-10-11 06:35:09 +0100

    Explain how to use script

--- generate_mp3_files.sh
+++ generate_mp3_files.sh
@@ -1,2 +1,6 @@
+# You can run this script like so:
+# BING_KEY=$BING_KEY bash -c "sh generate_mp3_files.sh"
+# Note that the script won't work without prepending BING_KEY, unless you export it in your shell config file (e.g. ~/.zshrc)
+
 docker build --build-arg BING_KEY=$BING_KEY -t mandarin .
 docker run -v $PWD/mp3_outputs:/app/mp3_outputs mandarin
